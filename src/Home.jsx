9b3f41eb50c899a69c206a64fcb4eed3de11e5ff
jael-VRR <jrodriguezruidias@gmail.com>
2021-08-24 01:47:14 -0500

    props coins

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,30 @@
-import {useEffect} from 'react';
-import axios from 'axios';
+import { useEffect,useState} from "react";
+import axios from "axios";
+
+import TableCoins from "./components/TableCoins.jsx";
 
 const Home = () => {
-    
-const getData=async()=>{
-    const res=await axios.get( "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
-    console.log(res)
-    
-} ;
-
-useEffect(() => {
-   getData()
-}, [])
-
-    return (
-        <div>
-            <h1>pagina principal</h1>
-        </div>
+  const [coins, setCoins] =useState([])
+  const getData = async () => {
+    const res = await axios.get(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
     );
+    console.log(res.data);
+    setCoins(res.data);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h1>pagina principal</h1>
+      <div>
+        <TableCoins coins={coins} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
