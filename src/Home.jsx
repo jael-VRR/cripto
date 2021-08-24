c9ccc75c8412ab06fc32134f20059a38d41c1b82
jael-VRR <jrodriguezruidias@gmail.com>
2021-08-24 00:45:22 -0500

    data

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import {useEffect} from 'react';
+import axios from 'axios';
 
 const Home = () => {
+    
+const getData=async()=>{
+    const res=await axios.get( "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+    console.log(res)
+    
+} ;
+
+useEffect(() => {
+   getData()
+}, [])
+
     return (
         <div>
-            
+            <h1>pagina principal</h1>
         </div>
     );
 };
