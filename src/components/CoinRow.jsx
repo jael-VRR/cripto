c7d1aaf6885bb3f404196102968af1576d7069f1
jael-VRR <jrodriguezruidias@gmail.com>
2021-08-24 04:43:50 -0500

    create coinRow

---
+++ src/components/CoinRow.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../styles/coinRow.css";
+
+const CoinRow = ({ coin, index }) => {
+  console.log(coin, index);
+  return (
+    <tr>
+      <td>{index} </td>
+      <td  className="icon-title">
+     <img src={coin.image} alt="coin" style={{width:"5%"}} className="me-4"/>
+        <span className="badge badge-light">{coin.name}</span>
+        <span className="ms-2 text-muted">{coin.symbol}</span>
+ 
+       
+      </td>
+      <td>{coin.current_price}</td>
+      <td>{coin.price_change_percentage_24h}</td>
+      <td>{coin.total_volume}</td>
+    </tr>
+  );
+};
+
+export default CoinRow;
