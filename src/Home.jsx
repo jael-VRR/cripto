c96c932804867489aa47b42bcfacb35432184846
jael-VRR <jrodriguezruidias@gmail.com>
2021-08-24 06:16:26 -0500

    add input search

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,12 @@
-import { useEffect,useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import TableCoins from "./components/TableCoins.jsx";
-import { Container } from 'reactstrap';
+import { Container } from "reactstrap";
 
 const Home = () => {
-  const [coins, setCoins] =useState([])
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
   const getData = async () => {
     const res = await axios.get(
       "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
@@ -18,10 +20,20 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <h1>pagina principal</h1>
+    <div className="wrapper-table">
+      <h1>Tabla crypto monedas</h1>
       <Container>
-        <TableCoins coins={coins} />
+        <div>
+          <input
+            type="text"
+            placeholder="Search a coin"
+            style={{ color: "white" }}
+            className="form-control bg-dark text-ligth text-center"
+            autoFocus
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+        <TableCoins coins={coins} search={search} />
       </Container>
     </div>
   );
