843c56c6de01b6e48992b9d6249d7f54ed1cfd61
jael-VRR <jrodriguezruidias@gmail.com>
2021-08-24 06:17:33 -0500

    add filter

--- src/components/CoinRow.jsx
+++ src/components/CoinRow.jsx
@@ -6,15 +6,24 @@ const CoinRow = ({ coin, index }) => {
   return (
     <tr>
       <td>{index} </td>
-      <td  className="icon-title">
-     <img src={coin.image} alt="coin" style={{width:"5%"}} className="me-4"/>
+      <td className="icon-title">
+        <img
+          src={coin.image}
+          alt="coin"
+          style={{ width: "5%" }}
+          className="me-4"
+        />
         <span className="badge badge-light">{coin.name}</span>
         <span className="ms-2 text-muted ">{coin.symbol}</span>
- 
-       
       </td>
       <td>{coin.current_price}</td>
-      <td>{coin.price_change_percentage_24h}</td>
+      <td
+        className={
+          coin.price_change_percentage_24h > 0 ? "text-success" : "text-danger"
+        }
+      >
+        {coin.price_change_percentage_24h}
+      </td>
       <td>{coin.total_volume}</td>
     </tr>
   );
