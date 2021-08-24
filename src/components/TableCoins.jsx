96866c3719d07cfd38cd983bbcce240b5822ee34
jael-VRR <jrodriguezruidias@gmail.com>
2021-08-24 06:18:46 -0500

    add function filter

--- src/components/TableCoins.jsx
+++ src/components/TableCoins.jsx
@@ -2,21 +2,25 @@ import React from "react";
 import { Table } from "reactstrap";
 import CoinRow from "./CoinRow";
 
-const titles=["#", "Coin", "Price", "Price Change", "24h volume"]
-const tableCoins = ({ coins }) => {
+const titles = ["#", "Coin", "Price", "Price Change", "24h volume"];
+const tableCoins = ({ coins, search }) => {
+  const filteredCoins = coins.filter((coin) =>
+  coin.name.toLowerCase().includes(search.toLowerCase())
+  
+);
+
   return (
     <Table dark>
       <thead>
         <tr>
-          {titles.map((title,i) =>(
+          {titles.map((title, i) => (
             <td key={i}>{title}</td>
           ))}
         </tr>
       </thead>
       <tbody>
-        {coins.map((coin,index) => (
-          <CoinRow coin={coin} index={index+1} key={index}/>
-       
+        {filteredCoins.map((coin, index) => (
+          <CoinRow coin={coin} index={index + 1} key={index} />
         ))}
       </tbody>
     </Table>
